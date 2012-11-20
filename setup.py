2f26b6e2a8885a84c07a7468228f2e15ec081470
Mathias Fussenegger <f.mathias@zignar.net>
2012-11-20 23:58:49 +0100

    add setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+
+
+setup(
+    name='easymail',
+    version='0.1.0',
+    author='Mathias Fussenegger',
+    author_email='pip@zignar.net',
+    url='http://pypi.python.org/pypi/easymail/',
+    license='LICENSE.txt',
+    description='abstraction layer on top of the email package to make sending\
+    emails a little bit easier',
+    py_modules=['easymail'],
+    install_requires=[
+    ]
+)
